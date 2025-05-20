834240aa94dbf3da29e9d696992cdfafe760054a
Ramin Khalili <Ramin Khalili>
2025-05-20 15:19:43 +0200

    start

---
+++ back-end/src/app.module.ts
@@ -0,0 +1,20 @@
+// app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class AppModule {}
